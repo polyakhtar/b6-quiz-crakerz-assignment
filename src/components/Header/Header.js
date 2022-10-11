c91a3eb01942a5ed3e1675dc767e347f869bc2f1
polyakhtar <poly09876pm@gmail.com>
2022-10-11 11:42:36 +0600

    header designed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className='bg-blue-500 w-100 flex justify-around items-center p-6'>
+            <div>
+                <h1 className='text-3xl font-bold'>QuizPanther</h1>
+            </div>
+            <div>
+            <Link className='mr-12  text-2xl' to='/'>Home</Link>
+            <Link className='mr-12  text-2xl' to='/statistics'>Statistics</Link>
+            <Link className='mr-12  text-2xl' to='/blog'>Blog</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div>
+            <div className='background-image-container'>
+               <div className='text-white m-40 p-8'>
+               <h1 className='text-4xl font-bold mb-4'>ARE YOU WANT TO CHECK YOUR SKILL?</h1>
+               <div className='mt-4'>
+               <p>This is the platform by which you will test your skill and develop your skill.</p>
+               <p>
+                In the present world skill is very important to survive modern world.</p>
+                </div>
+               </div>
+            </div>
+        </div>
+        
+        
+    );
+};
+
+export default Home;
