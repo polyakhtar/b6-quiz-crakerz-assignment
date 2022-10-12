9e05e56ef150cfbb9dd582a8696c988b38a1279c
polyakhtar <poly09876pm@gmail.com>
2022-10-12 21:14:46 +0600

    header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         
-        <nav className='bg-blue-500 w-100 flex justify-around items-center p-6'>
-            <div>
-                <h1 className='text-3xl font-bold'>QuizPanther</h1>
+        <nav className='bg-blue-500 flex flex-col md:flex-row md:space-x-8 md:justify-around p-8'>
+        
+            <div >
+                <h1 className='text-3xl font-bold mb-2'>QuizPanther</h1>
             </div>
             <div>
             <Link className='mr-12  text-2xl' to='/'>Home</Link>
